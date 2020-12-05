4be56e1678c74fef850c0f1d06a12e58b993a56b
Bazan Milla <victor_bazan_milla@optum.com>
2020-12-05 15:43:41 +0100

    Fix refactoring issues

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import Link from 'next/link';
+
 const Menu = () => (
+
     <nav className="flex items-center justify-between flex-wrap bg-gray-800 mb-6 p-6 rounded-md">
     <div className="w-full block flex-grow">
       <div className="md:flex-grow">
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -4,44 +4,86 @@ import withData from './withData';
 
 
 function SpeakersRenderProps(props) {
-  const speakers = [
-    {
-      imageSrc: 'speaker-component-1124',
-      name: 'Douglas Crockford',
-    },
-    {
-      imageSrc: 'speaker-component-1530',
-      name: 'Tamara Baker',
-    },
-    {
-      imageSrc: 'speaker-component-10803',
-      name: 'Eugene Chuvyrov',
-    },
-  ];
+  
 
   return props.children({
     speakers: speakers
   });
 }
 
-const Speakers = ({speakers}) => {
-  
+const Speakers = () => {
+  const speakers = [
+    {
+      imageSrc: 'speaker-component-1124',
+      name: 'Douglas Crockford',
+      id: 1124,
+      firstName: 'Douglas',
+      lastName: 'Crockford',
+      sat: true,
+      sun: false,
+      isFavorite: false,
+      bio:
+        'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
+    },
+    {
+      imageSrc: 'speaker-component-1530',
+      name: 'Tamara Baker',
+      id: 1530,
+      firstName: 'Tamara',
+      lastName: 'Baker',
+      sat: false,
+      sun: true,
+      isFavorite: true,
+      bio:
+        'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
+    },
+    {
+      imageSrc: 'speaker-component-10803',
+      name: 'Eugene Chuvyrov',
+      id: 10803,
+      firstName: 'Eugene',
+      lastName: 'Chuvyrov',
+      sat: true,
+      sun: false,
+      isFavorite: false,
+      bio:
+        'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
+    }
+  ];
   return (
-    <SpeakersRenderProps>
-      {() => {
-        
-        return (
           
           <div>
-          { speakers.map(( {name, imageSrc}) => {
-              return <img src={`images/${imageSrc}.png`} alt={name} key={imageSrc}></img>
-            }
-          )};
+            <div className="mb-6 ">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Search by name"
+              />
+            </div>
+            <div className="grid md:grid-cols-2 lg:grid-col-3 grid-cols-1 gap-12">
+              {speakers.map( ({ id, firstName, lastName, bio, isFavorite}) => (
+                <div className="rounded overflow-hidden shadow-lg p-6" key={id}>
+                  <div className="grid grid-cols-4 mb-6">
+                    <div className="font-bold text-lg col-span-3">
+                      {`${firstName} ${lastName}`}
+                    </div>
+                    <div className="flex justify-end">
+                      <div className={ isFavorite ? 'heartedbutton' : 'heartdarkbutton'}></div>
+                    </div>
+                  </div>
+                  <div className="mb-6">
+                    <img src={`/speakers/Speaker-${id}.jpg`} alt={`${firstName} ${lastName}`}  />
+                  </div>
+                  <div className="text-gray-600">
+                    {bio.substr(0, 70) + '... '}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
       )
-    }}
-  </SpeakersRenderProps>
-  );
+   
 };
 
-export default withData(Speakers);
+export default Speakers;
