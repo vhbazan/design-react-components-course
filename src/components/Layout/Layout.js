9d27f46ea9993a65dea178f2bdde6564786ec493
Bazan Milla <victor_bazan_milla@optum.com>
2020-12-05 14:09:11 +0100

    Refactor Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header/Header';
-import Menu from './Menu/Menu';
-import Footer from './Footer/Footer';
+import Header from '../Header/Header';
+import Menu from '../Menu/Menu';
+import Footer from '../Footer/Footer';
 
 const Layout = ({children}) => (
     <div className="mx-4 my-3">
