1887b75423859a898802ebb808776efef2e907fd
Bazan Milla <victor_bazan_milla@optum.com>
2020-12-05 14:09:35 +0100

    Add content to Header, Menu and Footer

--- pages/speakers.js
+++ pages/speakers.js
@@ -3,7 +3,7 @@ import Menu from '../src/components/Menu/Menu';
 import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar';
 import Speakers from '../src/components/Speakers/Speakers';
 import Footer from '../src/components/Footer/Footer';
-import Layout from '../src/components/Layout';
+import Layout from '../src/components/Layout/Layout';
 
 function Page() {
     return (
