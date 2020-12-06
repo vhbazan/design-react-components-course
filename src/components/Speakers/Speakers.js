2e4f0a10dce8caca304bb9a63d821163e3d25099
Bazan Milla <victor_bazan_milla@optum.com>
2020-12-06 19:26:59 +0100

    Hook Speakers with backend rest api

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,55 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SpeakerSearchBar from './SpeakerSearchBar';
 import Speaker from '../Speaker/Speaker';
+import axios from 'axios';
 
 const Speakers = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const speakersArray = [
-    {
-      imageSrc: 'speaker-component-1124',
-      name: 'Douglas Crockford',
-      id: 1124,
-      firstName: 'Douglas',
-      lastName: 'Crockford',
-      sat: true,
-      sun: false,
-      isFavorite: false,
-      bio:
-        'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
-    },
-    {
-      imageSrc: 'speaker-component-1530',
-      name: 'Tamara Baker',
-      id: 1530,
-      firstName: 'Tamara',
-      lastName: 'Baker',
-      sat: false,
-      sun: true,
-      isFavorite: true,
-      bio:
-        'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
-    },
-    {
-      imageSrc: 'speaker-component-10803',
-      name: 'Eugene Chuvyrov',
-      id: 10803,
-      firstName: 'Eugene',
-      lastName: 'Chuvyrov',
-      sat: true,
-      sun: false,
-      isFavorite: false,
-      bio:
-        'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
-    }
-  ];
-  const [speakers, setSpeakers] = useState(speakersArray);
   
-  function onFavoriteToggleHandler(speaker) {
+  const [speakers, setSpeakers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:4000/speakers');
+      setSpeakers(response.data);
+    }
+    fetchData();
+  }, []);
+
+  
+  async function onFavoriteToggleHandler(speaker) {
     console.log('onFavoriteToggleHandler clicked , ' )
     const updatedSpeaker = toggleSpeakerFavorite(speaker);
     const speakerIndex = speakers.map((speakerRec) => speakerRec.id).indexOf(speaker.id);
-  
+    await axios.put(`http://localhost:4000/speakers/${speaker.id}`, updatedSpeaker)
     setSpeakers([
       ...speakers.slice(0, speakerIndex), updatedSpeaker, ...speakers.slice(speakerIndex+1)
     ])
