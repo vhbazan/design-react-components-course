6c1efd930a8c063922bb333aeddd3e3f6a2c6e0d
vhbazan <vhbazanm@gmail.com>
2021-02-12 22:49:18 +0100

    Use useState and useEffect to load data

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,4 +1,4 @@
-import React, {  createContext, useState } from 'react';
+import React, {  createContext, useState, useEffect } from 'react';
 
 const DataContext = createContext();
 
@@ -38,10 +38,22 @@ const DataProvider = ({children}) => {
   ];
 
 
-  const state = {
+  const [state, setState] = useState({
     speakers: speakers,
-    status: 'success'
-  }
+    status: 'loading'
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setState({
+        speakers: speakers,
+        status: 'success',
+        error: undefined
+      })
+    }, 1000);
+    return () => clearTimeout(timer)
+  }, [])
+
       return (
         <DataContext.Provider value={state}>
             {children}
