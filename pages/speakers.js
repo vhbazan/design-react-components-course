a3c07ebb7733fc8747ea5d17a2cb7ddc5d5164fa
Victor H Bazan <vhbazanm@gmail.com>
2021-02-13 14:19:01 +0100

    Fix some minor css issues

--- pages/speakers.js
+++ pages/speakers.js
@@ -5,7 +5,7 @@ import Layout from '../src/components/Layout/Layout';
 function Page() {
     return (
        <Layout>
-            <Speakers bgColor="bg-gray-500"/>
+            <Speakers/>
         </Layout> 
     )
 }
--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -3,7 +3,7 @@ import SpeakerImage from './SpeakerImage';
 
 const Speaker = ({id, firstName, lastName, isFavorite, bio, onFavoriteToggle}) => {
     return (
-        <div className="rounded overflow-hidden shadow-lg p-6" key={id}>
+        <div className="rounded overflow-hidden shadow-lg p-6 bg-white" key={id}>
             <div className="grid grid-cols-4 mb-6">
             <div className="font-bold text-lg col-span-3">
                 {`${firstName} ${lastName}`}
