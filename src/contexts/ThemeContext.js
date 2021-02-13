49160092ee3695f77856db21311367511a5618bb
Victor H Bazan <vhbazanm@gmail.com>
2021-02-13 12:20:13 +0100

    Create ThemeContext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import useTheme from '../hooks/useTheme';
 
-const THEMELIST = {
-    DARK: 'dark',
-    LIGHT: 'light'
-}
-
 const ThemeContext = React.createContext();
 
-const ThemeProvider = ({children}) => {
-    const state = useTheme();
-    return (
-        <ThemeContext.Provider value={state}>
-            {children}
-        </ThemeContext.Provider>
-    )
-};
 
-export { ThemeContext, ThemeProvider, THEMELIST };
+const ThemeProvider = ({children}) => {
+  const state = {};
+
+  return (
+    <ThemeContext.Provider value={state}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+
+export {ThemeContext};
