c977a1c662ab3ecceab50df1dfeea08df976ba60
Victor H Bazan <vhbazanm@gmail.com>
2021-02-15 21:50:43 +0100

    Prepare for production

--- src/components/Speaker/SpeakerImage.js
+++ src/components/Speaker/SpeakerImage.js
@@ -3,7 +3,7 @@ import { SimpleImg } from 'react-simple-img';
 const SpeakerImage = ({id, firstName, lastName}) => {
 
     return (
-        <SimpleImg src={`/speakers/Speaker-${id}.jpg`}
+        <SimpleImg src={`/speakersimages/Speaker-${id}.jpg`}
             animationDuration="1"
             width={200}
             height={200}
